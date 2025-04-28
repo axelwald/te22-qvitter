e6952c1f55f2bfc583656f0188d9a06784d4aade
Axel Waldenborg <149167584+axelwald@users.noreply.github.com>
2025-04-28 09:27:40 +0200

    blabu

--- routes/tweets.js
+++ routes/tweets.js
@@ -33,4 +33,31 @@ router.post("/delete",
   res.redirect("/")
 })
 
+router.get("/:id/edit", async (req, res) => {
+  const id = req.params.id
+  if (!Number.isInteger(Number(id))) { return res.status(400).send("Invalid ID") }
+  const [rows] = await pool.promise().query("SELECT * FROM tweet WHERE id = ?", [id])
+  if (rows.length === 0) {
+    return res.status(404).send("Tweet not found")
+  }
+  res.render("edit.njk", { tweet: rows[0] })
+})
+
+router.post("/edit",
+  body("id").isInt(),
+  body("message").isLength({ min: 1, max: 130 }),
+  body("message").escape(),
+  async (req, res) => {
+  const errors = validationResult(req)
+  console.log(errors)
+  if (!errors.isEmpty()) { return res.status(400).send("Invalid input") }
+
+  const { id, message } = matchedData(req) // req.params.message varför inte?
+  const timestamp = new Date().toISOString().slice(0, 19).replace("T", " ")
+  console.log(timestamp)
+  await pool.promise().query("UPDATE tweet SET message = ?, updated_at = ? WHERE id = ?", [message, timestamp, id])
+  res.redirect("/")
+})
+
+
 export default router
